674d8b80070f85205f4e555907150ff51dbb6942
abdelhafid-dakhia <abdelhafid.dakhia@univ-biskra.dz>
2025-09-14 03:45:11 +0100

    Fix path-to-regexp error with catch-all route

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use("/api/session", sessionRoutes);
 
 // servir le build React
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.get("/*", (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
